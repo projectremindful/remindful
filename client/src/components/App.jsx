d76f353a3dfaf036d0f8f1e3bd8ddca44d2368ef
Matt Harrison <fittocode@gmail.com>
2019-02-26 22:12:04 +0100

    working on home page with transition

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,38 +5,51 @@ import Secret from './pages/Secret';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import api from '../api';
-import logo from '../logo.svg';
+import logo from '../images/logoonly.png';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       countries: []
-    }
+    };
     // api.loadUser();
   }
 
   handleLogoutClick(e) {
-    api.logout()
+    api.logout();
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">MERN Boilerplate</h1>
-          <NavLink to="/" exact>Home</NavLink>
+          {/* <NavLink to="/" exact>
+            Home
+          </NavLink>
           {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
           {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
-          {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
-          <NavLink to="/secret">Secret</NavLink>
+          {api.isLoggedIn() && (
+            <Link to="/" onClick={e => this.handleLogoutClick(e)}>
+              Logout
+            </Link>
+          )} */}
+          {/* <NavLink to="/secret">Secret</NavLink> */}
         </header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div class="spinner">
+          <div class="bounce1" />
+          <div class="bounce2" />
+          <div class="bounce3" />
+        </div>
+        <div id="pt-main" class="pt-perspective">
+          <div class="pt-page pt-page-1" />
+        </div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
-          <Route path="/secret" component={Secret} />
+          {/* <Route path="/secret" component={Secret} /> */}
           <Route render={() => <h2>404</h2>} />
         </Switch>
       </div>
