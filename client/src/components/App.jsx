b7a6210fa00b66895c5e72961ee4cd64b951fce5
Jengle <jennyrob282@gmail.com>
2019-02-27 10:22:07 +0100

    made Navbar component and tidied up client App.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import AllMemories from './pages/AllMemories';
 import Login from './pages/Login';
 import Reminder from './pages/Reminder';
 import Signup from './pages/Signup';
-import api from '../api';
-import logo from '../images/logoonly.png';
 import AddMemory from './pages/AddMemory';
+import Navbar from './Navbar';
 
 class App extends Component {
   constructor(props) {
@@ -18,42 +17,10 @@ class App extends Component {
     // api.loadUser();
   }
 
-  handleLogoutClick(e) {
-    api.logout();
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <NavLink to="/" exact>
-            Home
-          </NavLink>
-          {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
-          {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
-          {api.isLoggedIn() && (
-            <Link to="/" onClick={e => this.handleLogoutClick(e)}>
-              Logout
-            </Link>
-          )} */}
-          {/* <NavLink to="/secret">Secret</NavLink> */}
-          {api.isLoggedIn() && (
-            <NavLink to="/" onClick={e => this.handleLogoutClick(e)}>
-              Logout
-            </NavLink>
-          )}
-          {api.isLoggedIn() && <NavLink to="/allMemories">My memories</NavLink>}
-          {api.isLoggedIn() && <NavLink to="/AddMemory">Add Memory</NavLink>}
-        </header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="spinner">
-          <div class="bounce1" />
-          <div class="bounce2" />
-          <div class="bounce3" />
-        </div>
-        <div id="pt-main" class="pt-perspective">
-          <div class="pt-page pt-page-1" />
-        </div>
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/signup" component={Signup} />
@@ -63,6 +30,7 @@ class App extends Component {
           <Route path="/AddMemory" component={AddMemory} />
           <Route render={() => <h2>404</h2>} />
         </Switch>
+        
       </div>
     );
   }
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import logo from '../../images/logoonly.png';
 
 class Home extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
   render() {
     return (
       <div className="Home">
-        <h2 />
-        <p />
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="spinner">
+          <div className="bounce1" />
+          <div className="bounce2" />
+          <div className="bounce3" />
+        </div>
+        <div id="pt-main" className="pt-perspective">
+          <div className="pt-page pt-page-1"/>
+        </div>
       </div>
     );
   }
