ec269b146a0484575c15f7e90d46907007bf0286
Jengle <jennyrob282@gmail.com>
2019-02-27 16:50:45 +0100

    basic fucntionality on profile page done

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import api from '../../api';
-import { Button, CustomInput, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Button, CustomInput } from 'reactstrap'
 
 export default class Profile extends Component {
   constructor(props) {
@@ -18,24 +18,35 @@ export default class Profile extends Component {
       nostalgia: null,
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleChange() {
+  handleChange(key) {
     this.setState(prevState => ({
-      tranquility : !prevState.tranquility
+      [key] : !prevState[key]
     }))
   }
 
-  // switches load based on the user profile
-  // two things - one is to make the switches look differnet 
-  // other is to update the user on save
-  
-  
+  handleClick() {
+    const preferences = {
+      tranquility: this.state.tranquility,
+      empowerment: this.state.empowerment,
+      amusement: this.state.amusement,
+      inspiration: this.state.inspiration,
+      selfGrowth: this.state.selfGrowth,
+      motivation: this.state.motivation,
+      nostalgia: this.state.nostalgia,
+    }
+    // console.log(this.state._id, preferences)
+    api.updateUserPreferences(this.state._id, preferences)
+    .then(res => {
+      console.log('after the api', res)
+    })
+  }
+
   render() {
-    console.log(this.state.tranquility)
     return this.state.username ? 
     ( // when user information has loaded render this
-      // console.log(this.state.user.profileUrl)
       <div>
         <div>
           <img style={{height:"50px"}} src={this.state.profileUrl} alt="profile pic"/>
@@ -44,30 +55,60 @@ export default class Profile extends Component {
         </div>
         <hr/>
         <h2>Your Memory Preferences</h2>
-        <p></p>
-        <Form>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox"/>{' '}Calm
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox"/>{' '}Nostaligic
-            </Label>
-          </FormGroup>
-          <FormGroup>
-          <Label for="exampleCheckbox">Select the type of memories you want to reflect on</Label>
           <div>
-            <CustomInput checked={this.state.tranquility} onChange={e => this.handleChange("tranquility")} type="switch" id="exampleCustomSwitch" name="tranquility" label="tranquility" />
-            <CustomInput type="switch" id="exampleCustomSwitch2" name="customSwitch" label="Nostaliga" />
-            <CustomInput type="switch" id="exampleCustomSwitch3" label="But not this disabled one" disabled />
-          </div>
-        </FormGroup>
-          <Button>Save</Button>
-        </Form>
+            <CustomInput 
+              checked={this.state.tranquility} 
+              onChange={e => this.handleChange("tranquility")} 
+              type="switch" id="tranquility" 
+              name="tranquility" 
+              label="tranquility"
+            />
+             <CustomInput 
+              checked={this.state.empowerment} 
+              onChange={e => this.handleChange("empowerment")} 
+              type="switch" id="empowerment" 
+              name="empowerment" 
+              label="empowerment"
+            />
+             <CustomInput 
+              checked={this.state.amusement} 
+              onChange={e => this.handleChange("amusement")} 
+              type="switch" id="amusement" 
+              name="amusement" 
+              label="amusement"
+            />
+             <CustomInput 
+              checked={this.state.inspiration} 
+              onChange={e => this.handleChange("inspiration")} 
+              type="switch" id="inspiration" 
+              name="inspiration" 
+              label="inspiration"
+            />
+             <CustomInput 
+              checked={this.state.selfGrowth} 
+              onChange={e => this.handleChange("selfGrowth")} 
+              type="switch" id="selfGrowth" 
+              name="selfGrowth" 
+              label="selfGrowth"
+            />
+             <CustomInput 
+              checked={this.state.motivation} 
+              onChange={e => this.handleChange("motivation")} 
+              type="switch" id="motivation" 
+              name="motivation" 
+              label="motivation"
+            />
+             <CustomInput 
+              checked={this.state.nostalgia} 
+              onChange={e => this.handleChange("nostalgia")} 
+              type="switch" id="nostalgia" 
+              name="nostalgia" 
+              label="nostalgia"
+            />
+            <br/>      
+            <Button outline color="success" onClick={this.handleClick}>Save</Button>
+          </div>   
       </div>
-
     ) : // if user information has not yet loaded render this
     (
       <div>Loading</div>
@@ -78,7 +119,9 @@ export default class Profile extends Component {
     api.getProfile()
     .then(user => {
       this.setState({
+        _id: user._id,
         username: user.username,
+        profileUrl: user.profileUrl,
         tranquility: user.tranquility,
         empowerment: user.empowerment,
         amusement: user.amusement,
