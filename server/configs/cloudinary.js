2359cc8ad0bc63d1ea08175f58f43cf0bb730ae2
fbath <phoenixa@gmx.de>
2019-02-27 14:56:26 +0100

    added memory routes

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -10,7 +10,7 @@ cloudinary.config({
 
 var storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: 'memory-gallery', // The name of the folder in cloudinary
+  folder: 'remindful-gallery', // The name of the folder in cloudinary
   allowedFormats: ['jpg', 'png'],
   filename: function (req, file, cb) {
     cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
